e85887001e7a3c6678f9671620c4c0aba4d334da
Arius1121 <ariusaixan69@gmail.com>
2023-12-16 19:32:19 +0800

    mai naadha av muunuu

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="bg-[url(&quot;https://www.mmusic.mn/assets/img/demo/b2.jpg&quot;)]" > 
-       
+
+    <div className=" h-screen">
+    <div className="h-full w-72 bg-[#0C101B]">
+    <div className="h-80 w-80 bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ90OQrtuEA_WTZ89QNPWNwe3hGcjhzeNYtLg&usqp=CAU')]"> </div> 
+    </div>
+    
     </div>
   )
 }
