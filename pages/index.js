7b9787b749725e5662388f263ffdd212ea0c9b96
Arius1121 <ariusaixan69@gmail.com>
2023-12-16 23:36:44 +0800

    duu duucid hiisen

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +9,7 @@ export default function Home() {
 
     <div className=" h-screen">
     <div className="h-full w-72 bg-[#0C101B]">
-    <div className="h-80 w-80 bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ90OQrtuEA_WTZ89QNPWNwe3hGcjhzeNYtLg&usqp=CAU')]"> </div> 
+   
     </div>
     
     </div>
---
+++ pages/songs.js
@@ -0,0 +1,66 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+ return (
+    <div className="bg-[#131722]">
+        
+      <div className="flex justify-start justify-between ml-72"> 
+        <div className=" mb-4 text-white text-sm mt-20 font-bold"> Дуу </div>
+        <div className="mt-20 w-[450px] mr-56 rounded-xl h-10 bg-[#0C101B]"> </div>
+      </div>
+      <div className="ml-[270px] bg-[#0C101B] mt-20 h-8 w-[1418px] rounded flex justify-center ">
+        </div>
+      <div className="mt-2 mr-56 ml-72">
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+        <div className="bg-[#0C101B] mt-2 h-10 w-[1400px] rounded flex justify-center ">
+        </div>
+
+      </div>
+      
+    </div>
+ )
+}
