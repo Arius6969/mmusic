2fa48a4f0e89f9f7e75384c6846be24d513650ed
Arius1121 <ariusaixan69@gmail.com>
2023-12-17 22:20:00 +0800

    ariuspro

--- pages/album.js
+++ pages/album.js
@@ -8,158 +8,154 @@ export default function Home() {
     <div className="bg-white pb-24 dark:bg-[#131722]">
         
       <div className=" flex justify-start ml-72"> 
-        <button className="text-black text-sm mt-20 font-bold dark:text-white">Бүгд/ </button>
-        <button className="text-black text-sm mt-20 font-bold dark:text-white">Цомог/ </button>
-        <button className="text-black text-sm mt-20 font-bold dark:text-white">Сингл </button>  
-        <div className="absolute max-w-md mx-auto flex align-end ml-[610px] mt-[30px] text-white font-bold rounded-xl p-6">
-      <label htmlFor="numberInput" className="bg-zinc-500">
-      </label>
+        <button className="text-black text-sm mt-20 font-bold dark:text-[#E5E7EB]">Бүгд/ </button>
+        <button className="text-black text-sm mt-20 font-bold dark:text-[#E5E7EB]">Цомог/ </button>
+        <button className="text-black text-sm mt-20 font-bold dark:text-[#E5E7EB]">Сингл </button>  
+        <div className="absolute ml-[610px] mt-[30px] text-white font-bold rounded-2xl p-6">
       <input
-        className=" absolute bg-[#131722] ml-[300px] mt-1 p-2 w-[360px] border rounded w-full focus:border-[#131722] focus:transparent rounded-xl" 
+        className=" absolute bg-[#0C101B] ml-[300px] mt-1 p-2 w-[440px] rounded-xl" 
         placeholder="Хайх..."
       />
-    
-
     </div>
       </div>
       <div className="mt-8 grid grid-cols-6 gap-y-28 gap-x-14 mr-56 ml-72">
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/54a5fe2fc7496e0ada0d362c249f17a0/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
             <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Toos to Zoos </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Ginjin</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Toos to Zoos </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Ginjin</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/559264b51732d36f9055a37e1e0a30e7/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> ATM </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Ginjin</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> ATM </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Ginjin</span>
             </button>
         </div>
         <div className="bg-[url('https://i.scdn.co/image/ab67616d00001e029375810f4080113fa3323dd2')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white">  Хүний хүн </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Ginjin</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]">  Хүний хүн </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Ginjin</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/ca51f1ce23ba8b327d0ac2166a722d7d/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> BABA</div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Ginjin</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> BABA</div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Ginjin</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/4b59882e99ed2f7901afe60c85336bb0/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Бороо </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Ginjin</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Бороо </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Ginjin</span>
             </button>
         </div>
         <div className="bg-[url('https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/66/3e/dc/663edc3f-328b-640f-1ffa-f0cc9911a81d/8718857971337.png/1200x1200bf-60.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> HOT BOY</div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Ginjin</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> HOT BOY</div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Ginjin</span>
             </button>
         </div>
         <div className="bg-[url('https://m.media-amazon.com/images/I/51i5H7m+srL._SX354_SY354_BL0_QL100__UXNaN_FMjpg_QL85_.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Most HATED </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Vandebo</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Most HATED </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Vandebo</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/e4caed0cf5031f4929466a81a6df60d7/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white">  Баярлалаа </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Vandebo</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]">  Баярлалаа </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Vandebo</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/3a6548cd859ab33d737f9441bb8acb1c/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Munkhud 21 </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Vandebon</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Munkhud 21 </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Vandebon</span>
             </button>
             
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/0e2cb9494e36071491c8750ca3eef9cd/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> IN YOUR DREAM </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Vandebo</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> IN YOUR DREAM </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Vandebo</span>
             </button>
         </div> 
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/d29f3fcc1aa50b7fa786f45e8d50d021/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> дөнгөн данган </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Vandebo</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> дөнгөн данган </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Vandebo</span>
             </button>
         </div>
         <div className="bg-[url('https://static.qobuz.com/images/covers/9b/p9/o65sq05d7p99b_600.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> 3-тун </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Vandebo</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> 3-тун </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Vandebo</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/e679c857777c33b0e0848926584df215/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Нэг өдөр </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Thunderz</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Нэг өдөр </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Thunderz</span>
             </button>
         </div>
         <div className="bg-[url('https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/65/e5/c2/65e5c291-06da-ea99-619a-b407c92780cf/8720623491437.png/600x600bf-60.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Хатуу </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Thunderz</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Хатуу </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Thunderz</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/65713dea7d986ec48b1b77e5f04021b0/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Нисээрэй </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Thunderz</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Нисээрэй </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Thunderz</span>
             </button>
         </div>
         <div className="bg-[url('https://d15xdsbb8need2.cloudfront.net/images/music/albums/627b223c_87817a_1.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Street </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Thunderz</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Street </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Thunderz</span>
             </button>
         </div>
         <div className="bg-[url('https://i.ytimg.com/vi/lgOzRcsfyk8/maxresdefault.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> чи өөрөө </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Thunderz</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> чи өөрөө </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Thunderz</span>
             </button>
         </div>
         <div className="bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKpEhLkMkXO60lA0DGo4zTv1SD82qeYA_I4PYLG_zv0meUb3KcDVfql6HiNmF2wpCbeac&usqp=CAU')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Ноён эмэгтэй </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">Thunderz</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Ноён эмэгтэй </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">Thunderz</span>
             </button>
         </div>
         <div className="bg-[url('https://cdns-images.dzcdn.net/images/cover/173bafe1bb56b2fdbf220981e8b54573/264x264.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Халуун маруужин </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">NENE</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Халуун маруужин </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">NENE</span>
             </button>
         </div>
         <div className="bg-[url('https://d15xdsbb8need2.cloudfront.net/images/music/albums/63e222a2_bf7c20_1.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Okay </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">NENE</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Okay </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">NENE</span>
             </button>
         </div>
         <div className="bg-[url('https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/78/75/a2/7875a28f-82a6-9711-fdc0-8557c841a3cb/8721056097494.png/1200x1200bb.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Afterland </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">NENE</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Afterland </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">NENE</span>
             </button>
         </div>
         <div className="bg-[url('https://d15xdsbb8need2.cloudfront.net/images/music/albums/63e221fa_594833_1.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> 90on </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">NENE</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> 90on </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">NENE</span>
             </button>
         </div>
         <div className="bg-[url('https://d15xdsbb8need2.cloudfront.net/images/music/albums/63eaf3b8_7c6a75_1.jpg')] bg-cover h-44 w-44 rounded-xl cursor-pointer transition-all duration-500 hover:scale-110">
         <button className="mt-44 ">
-                <div className="text-black font-bold text-sm flex items-start dark:text-white"> Zuruhgui </div>
-                <span className="text-black flex items-start text-xs dark:text-white ">NENE</span>
+                <div className="text-black font-bold text-sm flex items-start dark:text-[#E5E7EB]"> Zuruhgui </div>
+                <span className="text-black flex items-start text-xs dark:text-[#E5E7EB] ">NENE</span>
             </button>
         </div>
 
